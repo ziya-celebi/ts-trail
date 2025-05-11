ee6a7d273dc2ae749da98b821193d4962b736fea
Ziya Celebi <ziyacelebi30@gmail.com>
2025-05-11 18:20:33 +0530

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+let sum: number = 0;
+const op: string | null = prompt("Enter an operation +, -, *, /, exit");
+const a: string | null = prompt("Enter your first number");
+const b: string | null = prompt("Enter your second number");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const numA = Number(a);
+const numB = Number(b);
+const oP = String(op);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+switch (oP) {
+  case "+":
+    sum = numA + numB;
+    console.log(sum);
+    break;
+  case "-":
+    sum = numA - numB;
+    console.log(sum);
+    break;
+  case "*":
+    sum = numA * numB;
+    console.log(sum);
+    break;
+  case "/":
+    sum = numA / numB;
+    console.log(sum);
+    break;
+}
